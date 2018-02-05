8b712514225f8a1038978336c613716e11c04aca
Nicola Critten <ucesncr@ucl.ac.uk>
2018-02-05 10:49:19 -0500

    Added 'birthday' icon and edited colours.

--- ucesncr/www/js/appActivity.js
+++ ucesncr/www/js/appActivity.js
@@ -1,3 +1,11 @@
 function menuClicked() {
-alert("You clicked the beach photo!");
+	alert("You clicked the beach photo!");
+}
+
+
+function cakeClicked() {	
+	var person = prompt("It's your birthday?\nPlease enter your name", "<Type Name Here>");
+
+	alert("Happy birthday to you\nHappy birthday to you \nHappy birthday dear "
+	+ person + "\nHappy birthday to you!");	
 }
